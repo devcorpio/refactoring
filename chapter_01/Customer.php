4876a2033879a93c1ca1bde346758305796a140c
Alberto Delgado <adelgado.roda@gmail.com>
2017-09-03 21:03:24 +0200

    chapter 1, I applied extract method, move method, and replace temp with query techniques

--- chapter_01/Customer.php
+++ chapter_01/Customer.php
@@ -27,67 +27,47 @@ class Customer
     }
 
     public function statement(): string {
-        /**
-         * @var double
-         */
-        $totalAmount = 0;
-
-        /**
-         * @var int
-         */
-        $frequentRenterPoints = 0;
-
         foreach($this->rentals as $rental) {
-            /**
-             * @var double
-             */
-            $thisAmount = 0;
-
             /**
              * @var string
              */
             $result = "Rental Record for ". $this->getName() . "\n";
 
-            //determine amounts for each line
-            switch($rental->getMovie()->getPriceCode()) {
-                case Movie::REGULAR:
-                    $thisAmount += 2;
-                    if ($rental->getDaysRented() > 2) {
-                        $thisAmount += ($rental->getDaysRented() - 2) * 1.5;
-                    }
-
-                    break;
-                case Movie::NEW_RELEASE:
-                    $thisAmount += $rental->getDaysRented() * 3;
-                    break;
-                case Movie::CHILDRENS:
-                    $thisAmount += 1.5;
-                    if ($rental->getDaysRented() > 3) {
-                        $thisAmount += ($rental->getDaysRented() - 3) * 1.5;
-                    }
-
-                    break;
-            }
-
-            $frequentRenterPoints++;
-
-            if (($rental->getMovie()->getPriceCode() == Movie::NEW_RELEASE) &&
-                $rental->getDaysRented() > 1) {
-
-                $frequentRenterPoints++;
-            }
-
             //show figures for this rental
-            $result .= "\t". $rental->getMovie()->getTitle() . "\t" . $thisAmount . "\n";
-            $totalAmount += $thisAmount;
-
+            $result .= "\t". $rental->getMovie()->getTitle() . "\t" . $rental->getCharge() . "\n";
         }
 
         //add footer lines
-        $result .= "Amount owed is " . $totalAmount . "\n";
-        $result .= "You earned " . $frequentRenterPoints . "frequent renter points";
+        $result .= "Amount owed is " . $this->getTotalCharge() . "\n";
+        $result .= "You earned " . $this->getTotalFrequentRenterPoints() . "frequent renter points";
 
         return $result;
 
+    }
+
+    private function getTotalCharge(): double {
+        /**
+         * @var double
+         */
+        $result = 0;
+
+        foreach($this->rentals as $rental) {
+            $result += $rental->getCharge();
+        }
+
+        return $result;
+    }
+
+    private function getTotalFrequentRenterPoints(): int {
+        /**
+         * @var int
+         */
+        $result = 0;
+
+        foreach($this->rentals as $rental) {
+            $result += $rental->getFrequentRenterPoints();
+        }
+
+        return $result;
     }
 }
