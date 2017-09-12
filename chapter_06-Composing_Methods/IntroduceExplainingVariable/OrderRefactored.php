c88f21071677c7aedff1cfb11bd85a22dbbe5dfe
Alberto Delgado <adelgado.roda@gmail.com>
2017-09-12 18:01:26 +0200

    Chapter 6: set quantityDiscount value as explanation variable

--- chapter_06-Composing_Methods/IntroduceExplainingVariable/OrderRefactored.php
+++ chapter_06-Composing_Methods/IntroduceExplainingVariable/OrderRefactored.php
@@ -15,8 +15,8 @@ class Order
     public function price(): double {
         // price is base price - quantity discount + shipping
         $basePrice = $this->quantity * $this->itemPrice;
+        $quantityDiscount = max(0, $this->quantity - 500) * $this->itemPrice * 0.05;
 
-        return $basePrice - max(0, $this->quantity - 500)
-            * $this->itemPrice * 0.05 + min($basePrice * 0.1, 100.0);
+        return $basePrice - $quantityDiscount + min($basePrice * 0.1, 100.0);
     }
 }
