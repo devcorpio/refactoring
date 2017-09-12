20c9ecca541cb4fc09e524ddf27021917e4451ff
Alberto Delgado <adelgado.roda@gmail.com>
2017-09-12 23:35:59 +0200

    Chapter 7: remove delegation method and use directly the method from type object

--- chapter_07-moving_features_between_objects/MoveMethod/Account.php
+++ chapter_07-moving_features_between_objects/MoveMethod/Account.php
@@ -18,9 +18,11 @@ class Account {
     public function bankCharge(): double {
         $result = 4.5;
         if ($this->daysOverdrawn > 0) {
-            $result += $this->overdraftCharge();
+            $result += $this->type->overdraftCharge($this->daysOverdrawn);
         }
 
         return $result;
     }
+
+
 }
