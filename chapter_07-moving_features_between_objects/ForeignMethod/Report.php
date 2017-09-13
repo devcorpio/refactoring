979f4284923c600b90f67b9dea640c62de08b78a
Alberto Delgado <adelgado.roda@gmail.com>
2017-09-13 14:27:09 +0200

    Chapter 7: code where we apply 'foreign method' technique

---
+++ chapter_07-moving_features_between_objects/ForeignMethod/Report.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * In that case all the syntax used is not real PHP,
+ * despite of, the concept I think is going to be understanded anyway
+ */
+
+class Report {
+    public function sendReport() {
+        $newStart = new DateTime(previousEnd.getYear(),
+            previousEnd.getMonth(),
+            previousEnd.getDate() + 1
+        );
+    }
+}
