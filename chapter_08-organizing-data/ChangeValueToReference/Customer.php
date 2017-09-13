2369cbb78691ea33bc53d04c7fa060ad451fed2a
Alberto Delgado <adelgado.roda@gmail.com>
2017-09-13 23:29:37 +0200

    code where we will apply 'change value to reference' technique

---
+++ chapter_08-organizing-data/ChangeValueToReference/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+class Customer {
+    /**
+     * @var string
+     */
+    private $name;
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+}
