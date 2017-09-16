df204a673629ba8851693f1227edc09114bceaa0
Alberto Delgado <adelgado.roda@gmail.com>
2017-09-16 13:59:17 +0200

    Chapter 9: first step of the technique 'consolidate conditional expressions' with OR

--- chapter_09-symplifying_conditionals_expressions/ConsolidateConditionalExpression/ExampleWIthOrsRefactored.php
+++ chapter_09-symplifying_conditionals_expressions/ConsolidateConditionalExpression/ExampleWIthOrsRefactored.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alberto
- * Date: 16/09/17
- * Time: 13:57
- */
+
+class ExampleWithOrs {
+    /**
+     * @var int
+     */
+    private $seniority;
+
+    /**
+     * @var int
+     */
+    private $monthsDisabled;
+
+    /**
+     * @var bool
+     */
+    private $isPartTime;
+
+    public function disabilityAmount() {
+        if ($this->seniority < 2 || $this->monthsDisabled > 12 || $this->isPartTime) {
+            return 0;
+        }
+
+        echo 'doing something';
+    }
+}
