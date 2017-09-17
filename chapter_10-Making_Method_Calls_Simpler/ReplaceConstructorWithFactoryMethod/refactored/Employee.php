0034865fc972cab19adc8b74858930a249514e05
Alberto Delgado <adelgado.roda@gmail.com>
2017-09-17 22:45:14 +0200

    Chapter 10: technique applied

--- chapter_10-Making_Method_Calls_Simpler/ReplaceConstructorWithFactoryMethod/refactored/Employee.php
+++ chapter_10-Making_Method_Calls_Simpler/ReplaceConstructorWithFactoryMethod/refactored/Employee.php
@@ -10,7 +10,25 @@ class Employee {
      */
     private $type;
 
-    public function __construct(int $type) {
+    private function __construct(int $type) {
         $this->type = $type;
     }
+
+    public static function create(int $type) {
+        switch ($type) {
+            case self::ENGINEER:
+                return new Engineer();
+                break;
+            case self::SALESMAN:
+                return new Salesman();
+                break;
+            case self::MANAGER:
+                return new Manager();
+                break;
+            default:
+                throw new InvalidArgumentException("Incorrect type code value");
+        }
+    }
 }
+
+$employer = Employee::create(Employee::ENGINEER);
